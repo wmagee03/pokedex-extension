ab98a1f30c6d616fd19fdff74ee1fb6ed1feb49c
Sam Parsons <samparsons269@gmail.com>
2019-07-31 20:03:35 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+import Searchbar from './Components/Searchbar';
+
+class App extends React.Component {
+  state = {
+    searchValue: "",
+    searchPlaceholder: "Enter song title",
+    resultsArr: [],
+    resultsSong: "",
+    loading: false,
+    songList: [],
+    typeaheadRef: {}
+  };
+
+  componentDidMount = () => {
+    fetch("https://realbookindex-api.herokuapp.com/", {
+      method: "GET",
+      mode: "cors"
+    })
+      .then(res => res.json())
+      .then(res => this.setState({ songList: res.data }, () => {}));
+  };
+
+  handleSearch = e => {
+    this.setState({ searchValue: e }, () => console.log(e));
+  };
+
+  handleSelect = e => {
+    console.log(e);
+    this.setState({ searchValue: e }, () => console.log(e));
+  };
+
+  handleSubmit = e => {
+    console.log(typeof this.state.searchValue);
+    this.toggleLoading();
+    fetch("https://realbookindex-api.herokuapp.com/", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        title:
+          typeof this.state.searchValue === "object"
+            ? this.state.searchValue.toString()
+            : this.state.searchValue
+      })
+    })
+      // .then(res => console.log(res.json()));
+      .then(res => res.json())
+      .then(res =>
+        this.setState(
+          {
+            resultsArr: res.data.length > 0 ? res.data : [],
+            resultsSong: res.data.length > 0 ? res.data[0].title : "Not Found",
+            searchValue: ""
+          },
+          () => {
+            this.toggleLoading();
+          }
+        )
+      );
+
+    e.preventDefault();
+  };
+
+  handleClear = () => {
+    this.setState({ resultsArr: [], resultsSong: "", searchValue: "" }, () => {
+      this.handleSearch("");
+    });
+  };
+
+  toggleLoading = () => this.setState({ loading: !this.state.loading });
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Pokedex</h1>
+          <Searchbar
+            handleSubmit={this.handleSubmit}
+            handleSearch={this.handleSearch}
+            handleSelect={this.handleSelect}
+            searchValue={this.state.searchValue}
+            loading={this.state.loading}
+            songList={this.state.songList}
+          />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Searchbar.js
@@ -0,0 +1,132 @@
+import React from "react";
+import styled from "styled-components";
+import Spinner from "react-bootstrap/Spinner";
+import InputGroup from "react-bootstrap/InputGroup";
+// import FormControl from "react-bootstrap/FormControl";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { Typeahead } from "react-bootstrap-typeahead";
+
+const Styles = styled.div`
+  .hidden {
+    display: none;
+  }
+  .text {
+    padding-left: 13px;
+  }
+
+  .dropdown-menu {
+    background: #777;
+  }
+  .dropdown-item {
+    color: #fff;
+  }
+  .dropdown-item:hover {
+    color: #000;
+  }
+
+  @media (max-width: 480px) {
+    .form-control {
+      font-size: 20px;
+    }
+
+    .btn {
+      font-size: medium !important;
+    }
+
+    .mb-3 {
+      width: 90vw;
+    }
+  }
+`;
+
+class Searchbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.typeahead = React.createRef();
+  }
+
+  render() {
+    return (
+      <Styles>
+        <div className="main">
+          <Form
+            onSubmit={e => {
+              this.props.handleSubmit(e);
+              this.typeahead.getInstance().clear();
+            }}
+          >
+            <InputGroup className="mb-3" size="lg">
+              <Typeahead
+                id="typeahead"
+                labelKey="song"
+                options={this.props.songList}
+                onInputChange={this.props.handleSearch}
+                onChange={this.props.handleSelect}
+                minLength={3}
+                bsSize="large"
+                placeholder="Enter song title..."
+                highlightOnlyResult
+                selectHintOnEnter
+                autoFocus
+                ref={typeahead => (this.typeahead = typeahead)}
+              />
+              <InputGroup.Append>
+                <Button variant="outline-secondary" type="submit">
+                  <Spinner
+                    as="span"
+                    animation="border"
+                    size="sm"
+                    role="status"
+                    aria-hidden="true"
+                    className={this.props.loading ? "" : "hidden"}
+                  />
+                  <span className="sr-only">Loading...</span>
+                  <span className="text">Search</span>
+                </Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Form>
+        </div>
+      </Styles>
+    );
+  }
+}
+
+// const Searchbar = props => (
+//   <Styles>
+//     <div className="main">
+//       <Form onSubmit={props.handleSubmit}>
+//         <InputGroup className="mb-3" size="lg">
+//           <Typeahead
+//             id="typeahead"
+//             labelKey="song"
+//             options={props.songList}
+//             onInputChange={props.handleSearch}
+//             onChange={props.handleSelect}
+//             minLength={2}
+//             bsSize="large"
+//             placeholder="Enter song title..."
+//             ref={typeahead => console.log(typeahead)}
+//           />
+//           <InputGroup.Append>
+//             <Button variant="outline-secondary" type="submit">
+//               <Spinner
+//                 as="span"
+//                 animation="border"
+//                 size="sm"
+//                 role="status"
+//                 aria-hidden="true"
+//                 className={props.loading ? "" : "hidden"}
+//               />
+//               <span className="sr-only">Loading...</span>
+//               <span className="text">Search</span>
+//             </Button>
+//           </InputGroup.Append>
+//         </InputGroup>
+//       </Form>
+//     </div>
+//   </Styles>
+// );
+
+export default Searchbar;
